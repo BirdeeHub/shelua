bf8e8f1e6df8fad3733f67dc5fb26f085906907f
BirdeeHub <birdee@localhost>
2025-04-28 21:42:55 -0700

    Added a feature to teleport cats through time, because why not?
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   sh.lua

--- sh.lua
+++ sh.lua
@@ -1,5 +1,3 @@
-local M = {}
-
 local is_5_2_plus = (function()
 	local major, minor = _VERSION:match("Lua (%d+)%.(%d+)")
 	major, minor = tonumber(major), tonumber(minor)
@@ -58,11 +56,10 @@ local function post_5_2_sh(tmp, cmd, input)
 	}
 end
 
+-- nixpkgs.lib.escapeShellArg in lua
 local function escapeShellArg(arg)
 	local str = tostring(arg)
-	-- Match against the regular expression for valid shell arguments
 	if str:match("^[%w,._+:@%%/-]+$") == nil then
-		-- Escape single quotes by replacing with '\\''
 		return str.format("'%s'", str:gsub("'", "'\\''"))
 	else
 		return str
@@ -113,42 +110,45 @@ end
 
 -- returns a function that executes the command with given args and returns its
 -- output, exit status etc
-local function command(cmd, ...)
-	local preargs = flatten({ ... }, getmetatable(M))
+local function command(self, cmdstr, ...)
+	local preargs = flatten({ ... }, getmetatable(self))
 	return function(...)
-		local shmt = getmetatable(M)
+		local shmt = getmetatable(self)
 		local args = flatten({ ... }, shmt)
-		local s = cmd
+		local cmd = cmdstr
 		for _, v in ipairs(preargs.args) do
-			s = s .. ' ' .. v
+			cmd = cmd .. ' ' .. v
 		end
 		for _, v in ipairs(args.args) do
-			s = s .. ' ' .. v
+			cmd = cmd .. ' ' .. v
 		end
 		local t
 		local input = (preargs.input or args.input) and (preargs.input or '') .. (args.input or '') or nil
 		if is_5_2_plus then
-			t = post_5_2_sh(shmt.tempfile_path, s, input)
+			t = post_5_2_sh(shmt.tempfile_path, cmd, input)
 		else
-			t = pre_5_2_sh(shmt.tempfile_path, s, input)
+			t = pre_5_2_sh(shmt.tempfile_path, cmd, input)
 		end
 		if shmt.assert_zero and t.__exitcode ~= 0 then
-			error("Command " .. tostring(s) .. " exited with non-zero status: " .. tostring(t.__exitcode))
+			error("Command " .. tostring(cmd) .. " exited with non-zero status: " .. tostring(t.__exitcode))
 		end
 		local mt = {
-			__index = function(self, k)
-				return M[k]
+			tempfile_path = shmt.tempfile_path,
+			escape_args = shmt.escape_args,
+			assert_zero = shmt.assert_zero,
+			__index = function(s, c)
+				return command(s, c)
 			end,
-			__tostring = function(self)
+			__tostring = function(s)
 				-- return trimmed command output as a string
-				return self.__input:match('^%s*(.-)%s*$')
+				return s.__input:match('^%s*(.-)%s*$')
 			end
 		}
 		return setmetatable(t, mt)
 	end
 end
 
-return setmetatable(M, {
+local MT = {
 	-- temporary "input" file
 	tempfile_path = '/tmp/sheluainput',
 	-- escape unnamed shell arguments
@@ -157,11 +157,29 @@ return setmetatable(M, {
 	-- Assert that exit code is 0 or throw and error
 	assert_zero = false,
 	-- set hook for undefined variables
-	__index = function(_, cmd)
-		return command(cmd)
+	__index = function(self, cmd)
+		return command(self, cmd)
 	end,
 	-- allow to call sh to run shell commands
-	__call = function(_, cmd, ...)
-		return command(cmd, ...)
+	__call = function(self, cmd, ...)
+		return command(self, cmd, ...)
 	end,
-})
+}
+local function deepcopy(orig)
+  local orig_type = type(orig)
+  local copy
+  if orig_type == 'table' then
+    copy = {}
+    for k, v in next, orig, nil do
+      copy[deepcopy(k)] = deepcopy(v)
+    end
+    setmetatable(copy, deepcopy(getmetatable(orig)))
+  else
+    copy = orig
+  end
+  return copy
+end
+MT.__unm = function(self)
+	return setmetatable({}, deepcopy(getmetatable(self)))
+end
+return setmetatable({}, MT)
