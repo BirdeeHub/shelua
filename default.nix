7d0c826374b572c523e93f9d6ca6dcbd1003569b
BirdeeHub <birdee@localhost>
2025-04-29 07:15:59 -0700

    refactor(nix): removed unnecessary pipe

--- default.nix
+++ default.nix
@@ -3,11 +3,9 @@
   lua,
   luapkgs ? lua.pkgs,
   ...
-}: let
-  pipe = builtins.foldl' (x: f: f x);
-in luapkgs.luaLib.toLuaModule (runCommand "shelua" {
+}: luapkgs.luaLib.toLuaModule (runCommand "shelua" {
   src = ./lua/sh.lua;
-  env_path = with builtins; pipe luapkgs.lua.LuaPathSearchPaths [ head (split "[\/][?]") head ];
+  env_path = with builtins; (head (split "[\/][?]" (head luapkgs.lua.LuaPathSearchPaths)));
 } /*bash*/ ''
   mkdir -p "$out/$env_path"
   cp "$src" "$out/$env_path/sh.lua"
